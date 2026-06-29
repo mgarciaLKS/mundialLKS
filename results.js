9844ca0c21ed6a05b97e25919e608e70a39145a1
mgarciaLKS <mgarcia@lksnext.com>
2026-06-29 10:39:32 +0200

    Update results.js to include Canada in round16 and modify script reference in index.html

--- results.js
+++ results.js
@@ -76,7 +76,7 @@ const RESULTS = {
       "Bosnia y Herzegovina", "Paraguay", "Ecuador", "Suecia",
       "Senegal", "Argelia", "RD del Congo", "Ghana"
     ],
-    round16: [],
+    round16: ["Canadá"],
     quarterfinals: [],
     semifinals: [],
 
