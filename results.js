a151249d00f21ba724dbfd96b359201eabb0c888
mgarciaLKS <mgarcia@lksnext.com>
2026-06-29 12:13:43 +0200

    Update script reference in index.html and expand match details in results.js

--- results.js
+++ results.js
@@ -90,8 +90,38 @@ const RESULTS = {
 
     matches: {
       round32: [
-        // Partido 73: Sudáfrica (2º A) vs Canadá (2º B)
-        {match: 73, team1: "Sudáfrica", team2: "Canadá", winner: "Canadá"}
+        // Partido 73: 2ºA vs 2ºB
+        {match: 73, team1: "Sudáfrica",      team2: "Canadá",              winner: "Canadá"},
+        // Partido 74: 1ºE vs 3ºD
+        {match: 74, team1: "Alemania",        team2: "Paraguay",            winner: ""},
+        // Partido 75: 1ºF vs 2ºC
+        {match: 75, team1: "Países Bajos",    team2: "Marruecos",           winner: ""},
+        // Partido 76: 1ºC vs 2ºF
+        {match: 76, team1: "Brasil",          team2: "Japón",               winner: ""},
+        // Partido 77: 1ºI vs 3ºF
+        {match: 77, team1: "Francia",         team2: "Suecia",              winner: ""},
+        // Partido 78: 2ºE vs 2ºI
+        {match: 78, team1: "Costa de Marfil", team2: "Noruega",             winner: ""},
+        // Partido 79: 1ºA vs 3ºE
+        {match: 79, team1: "México",          team2: "Ecuador",             winner: ""},
+        // Partido 80: 1ºL vs 3ºK
+        {match: 80, team1: "Inglaterra",      team2: "RD del Congo",        winner: ""},
+        // Partido 81: 1ºD vs 3ºB
+        {match: 81, team1: "Estados Unidos",  team2: "Bosnia y Herzegovina",winner: ""},
+        // Partido 82: 1ºG vs 3ºI
+        {match: 82, team1: "Bélgica",         team2: "Senegal",             winner: ""},
+        // Partido 83: 2ºK vs 2ºL
+        {match: 83, team1: "Portugal",        team2: "Croacia",             winner: ""},
+        // Partido 84: 1ºH vs 2ºJ
+        {match: 84, team1: "España",          team2: "Austria",             winner: ""},
+        // Partido 85: 1ºB vs 3ºJ
+        {match: 85, team1: "Suiza",           team2: "Argelia",             winner: ""},
+        // Partido 86: 1ºJ vs 2ºH
+        {match: 86, team1: "Argentina",       team2: "Cabo Verde",          winner: ""},
+        // Partido 87: 1ºK vs 3ºL
+        {match: 87, team1: "Colombia",        team2: "Ghana",               winner: ""},
+        // Partido 88: 2ºD vs 2ºG
+        {match: 88, team1: "Australia",       team2: "Egipto",              winner: ""}
       ],
       round16: [],
       quarterfinals: [],
