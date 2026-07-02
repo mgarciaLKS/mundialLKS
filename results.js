63199c3e6a6daac76b6fe1c1c6b5b2830ab3e61d
Mikel Garcia <mgarcia@lksnext.com>
2026-07-02 10:14:56 +0200

    Update match winners in results.js

--- results.js
+++ results.js
@@ -105,11 +105,11 @@ const RESULTS = {
         // Partido 79: 1ºA vs 3ºE
         {match: 79, team1: "México",          team2: "Ecuador",             winner: "México"},
         // Partido 80: 1ºL vs 3ºK
-        {match: 80, team1: "Inglaterra",      team2: "RD del Congo",        winner: ""},
+        {match: 80, team1: "Inglaterra",      team2: "RD del Congo",        winner: "Inglaterra"},
         // Partido 81: 1ºD vs 3ºB
-        {match: 81, team1: "Estados Unidos",  team2: "Bosnia y Herzegovina",winner: ""},
+        {match: 81, team1: "Estados Unidos",  team2: "Bosnia y Herzegovina",winner: "Estados Unidos"},
         // Partido 82: 1ºG vs 3ºI
-        {match: 82, team1: "Bélgica",         team2: "Senegal",             winner: ""},
+        {match: 82, team1: "Bélgica",         team2: "Senegal",             winner: "Bélgica"},
         // Partido 83: 2ºK vs 2ºL
         {match: 83, team1: "Portugal",        team2: "Croacia",             winner: ""},
         // Partido 84: 1ºH vs 2ºJ
